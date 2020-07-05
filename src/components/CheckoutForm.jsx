e5541a619e5130999b1e94724889f221385df002
Gerard C. Saperas <gcsaperas@gmail.com>
2020-07-05 14:48:54 +0200

    added clg to client secret to see whats happening

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -33,6 +33,8 @@ export default function CheckoutForm(props) {
 				return res.json();
 			})
 			.then((data) => {
+				console.log(data.clientSecret);
+				console.log(data);
 				setClientSecret(data.clientSecret);
 			});
 	}, []);
